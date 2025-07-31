b927e4fcbd97836608a42185393907a62e134248
ahmedovdeveloper <aafa45720@gmail.com>
2025-07-31 11:25:16 +0500

    all code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const cors = require('cors');
-app.use(cors(["*"]));
+app.use(cors({ origin: '*' }));
 
 const imageSchema = new mongoose.Schema({
   filename: String,
